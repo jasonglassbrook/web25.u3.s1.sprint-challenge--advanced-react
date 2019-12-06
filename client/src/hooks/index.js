93fe61db3b9ed6eecbf5981da5942f55ad085530
o1123516766542617 <o1123516766542617@outlook.com>
2019-12-06 13:37:10 -0500

    add module index for hooks

---
+++ client/src/hooks/index.js
@@ -0,0 +1,14 @@
+/*******************************************************************************
+  HOOKS
+*******************************************************************************/
+
+/// imports ///
+import remote from './remote';
+
+/// exports ///
+export default {
+  remote
+};
+
+export * from './remote';
+export {default as remote} from './remote';
