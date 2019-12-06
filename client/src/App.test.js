d2b68406a641328eabb708002912f64b9ee6cb39
o1123516766542617 <o1123516766542617@outlook.com>
2019-12-06 11:39:39 -0500

    apply my project template

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+/// components ///
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+/// testers ///
+import renders from 'testers/renders';
+
+/***************************************
+  TESTING
+***************************************/
+const what = {
+  name : 'App',
+  Component : App,
+};
+
+renders (what.name, what.Component);
