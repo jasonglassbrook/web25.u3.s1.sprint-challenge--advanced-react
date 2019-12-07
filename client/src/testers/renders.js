5681a49af73f7dc4e886e050ab70109c8cfb6fa9
o1123516766542617 <o1123516766542617@outlook.com>
2019-12-07 11:14:24 -0500

    update testers/renders to accept render props

--- client/src/testers/renders.js
+++ client/src/testers/renders.js
@@ -2,12 +2,15 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 
+/// tools ///
+import { is } from 'tools/iffy';
+
 /***************************************
   TEST
 ***************************************/
-export const renders = (name, Component) => {
+export const renders = (name, Component, props) => {
   test (`${name} renders without crashing`, () => {
-    const thing = render (<Component/>);
+    const thing = render (<Component {...(is (props) ? props : {})}/>);
     
     thing.unmount ();
   });
