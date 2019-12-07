6d703a26935d60284091677eb1556b33aede2b96
o1123516766542617 <o1123516766542617@outlook.com>
2019-12-07 12:23:15 -0500

    add NavLinks

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,14 @@
 import React from 'react';
 
 /// components ///
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import AppAsClass from 'AppAsClass';
-import AppAsFun from 'AppAsFun';
+import {
+  BrowserRouter as Router,
+  Route,
+  NavLink,
+} from 'react-router-dom';
+import AppAsClass from './AppAsClass';
+import AppAsFun from './AppAsFun';
+import FlexRow from 'components/basics/flex/FlexRow';
 
 /// styles ///
 import './styles/App.css';
@@ -17,21 +22,25 @@ const init = {
 };
 
 const App = (props) => (
-  <div className='App'>
-    <header>
-      <h1>sprint-challenge--advanced-react</h1>
-    </header>
-    <main>
-      <Router>
-        <Route path='/as-class'>
-          <AppAsClass api={init.api}/>
-        </Route>
-        <Route path='/as-fun'>
-          <AppAsFun api={init.api}/>
-        </Route>
-      </Router>
-    </main>
-  </div>
+  <Router>
+    <div className='App'>
+      <header>
+        <h1>sprint-challenge--advanced-react</h1>
+        <nav>
+          <NavLink to='/as-class'>View As Class Component</NavLink>
+          <NavLink to='/as-fun'>View As Function Component</NavLink>
+        </nav>
+      </header>
+      <main>
+          <Route path='/as-class'>
+            <AppAsClass api={init.api}/>
+          </Route>
+          <Route path='/as-fun'>
+            <AppAsFun api={init.api}/>
+          </Route>
+      </main>
+    </div>
+  </Router>
 );
 
 /**************************************/
