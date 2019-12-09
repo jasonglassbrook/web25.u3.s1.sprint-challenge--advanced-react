0152a9c4db12e2a096878baee5e0e6a049030e57
o1123516766542617 <o1123516766542617@outlook.com>
2019-12-09 16:15:17 -0500

    update to use klaxios.request

--- client/src/tools/klaxios/get.js
+++ client/src/tools/klaxios/get.js
@@ -1,59 +1,18 @@
 /*******************************************************************************
-  get - A wrapper for `axios.get`.
+  get
+--------------------------------------------------------------------------------
+  A wrapper for `axios.get`.
+  It has less customizability than `axios`, but handles most basic cases.
 *******************************************************************************/
 
-/// external modules ///
-import axios from 'axios';
-
-/// tools ///
-// import $tree from 'tools/$tree';
-// import { flag as _flag } from 'tools/hi';
-import { is } from 'tools/iffy';
-
 /// internal modules ///
-// import $trunk from './$';
-import handleAxiosResponse from './handleAxiosResponse';
-import handleAxiosError from './handleAxiosError';
-
-/*//////////////////////////////////////
-  meta
-//////////////////////////////////////*/
-// const $this = $tree.branch ('get', $trunk);
-// const flag = (method, message) => {
-//   _flag (method, `${$this.$fullname} : ${message}`);
-// };
-
-/***************************************
-  INIT
-***************************************/
-const init = {
-  'options' : {
-    'fallbackData' : {},
-    'handleResponse' : handleAxiosResponse,
-    'handleError' : handleAxiosError,
-  },
-};
+import request from './request';
 
 /***************************************
   MAIN
 ***************************************/
-export const get = (address, setData, options) => {
-  /// apply defaults ///
-  if (is (options)) {
-    options = {...init.options, ...options};
-  } else {
-    options = init.options;
-  }
-
-  /// do it ///
-  axios
-    .get (address)
-    .then ((response) => {
-      options.handleResponse (response, setData, options.fallbackData);
-    })
-    .catch ((error) => {
-      options.handleError (error, setData, options.fallbackData);
-    });
+export const get = (url, options) => {
+  request ('get', url, options)
 };
 
 /**************************************/
